e71dea4b00b0c91189be2d9f5b7d87c6dff61697
Min Ko Naing <mkn.minko.naing99@gmail.com>
2025-06-09 07:17:22 +0700

    first commit

---
+++ basic.js
@@ -0,0 +1,14 @@
+console.table({ name: "alex" });
+
+//clg -> console.log(first)
+
+//pseudo
+
+// (reserved keyward)(variable name ) = (type of value)
+
+let name = "naing";
+
+console.log(name);
+// 41:22 https://www.youtube.com/watch?v=H3XIJYEPdus&ab_channel=HuXnWebDev
+
+console.log("Hello World");
