a9c2f0edfd4eb615a904ea140c24d3c8bd3e1a7b
Min Ko Naing <mkn.minko.naing99@gmail.com>
2025-08-03 09:28:16 +0700

    template literals

---
+++ app/app.js
@@ -0,0 +1,11 @@
+// Template Literals
+// const firstName = "FirstName";
+// const lastName = "LastName";
+// console.log(`My name is: ${firstName} ${lastName}`);
+
+// function info() {
+//   return "My name is something and some year old";
+// }
+
+// console.log(`${info()}`);
+// -----------------------------------
