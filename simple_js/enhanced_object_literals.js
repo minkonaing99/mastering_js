fd8a6f85835b39770b4756eb422d320a8738a95b
Min Ko Naing <mkn.minko.naing99@gmail.com>
2025-08-03 10:02:41 +0700

    new changes

---
+++ simple_js/enhanced_object_literals.js
@@ -0,0 +1,44 @@
+// function user(name, age, work) {
+//   return {
+//     name: name,
+//     age: age,
+//     work: work,
+//     intro: function () {
+//       console.log(`My name is ${name} & I am ${work}`);
+//     },
+//   };
+// }
+
+// function user(name, age, work) {
+//   return {
+//     name,
+//     age,
+//     work,
+//     intro() {
+//       console.log(`My name is ${name} & I am ${work}`);
+//     },
+//   };
+// }
+
+// const thomas = user("Thomas", 25, "WebDev");
+// console.log(thomas.intro());
+
+// Problems
+
+// function number(a, b, c) {
+//   return {
+//     a,
+//     b,
+//     c,
+//   };
+// }
+
+// const num1 = number(1, 2, 3);
+// console.log(num1);
+
+const a = 1;
+const b = 2;
+const c = 3;
+
+const obj = { a, b, c };
+console.log(obj);
