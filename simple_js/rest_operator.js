649413493d74c8a5c1b5105a3b3ddbdf3e663819
“minkonaing”99 <mkn.minko.naing99@gmail.com>
2025-08-04 19:54:46 +0700

    spread and rest

---
+++ simple_js/rest_operator.js
@@ -0,0 +1,2 @@
+// The rest parameter syntax allows a funciton to accept an indefinite number of arguments
+//as an array, providing a way to represent variadic functions in javascripts.
--- simple_js/spread_operator.js
+++ simple_js/spread_operator.js
@@ -15,10 +15,41 @@
 // giveMe4(...colors);
 // console.log(...colors);
 
-const strNums = ["one", "two", "three"];
-const moreStrNums = ["four", "five", "six"];
+// const strNums = ["one", "two", "three"];
+// const moreStrNums = ["four", "five", "six"];
 
-const concat = [...strNums, ...moreStrNums];
-console.log(concat);
+// const concat = [...strNums, ...moreStrNums];
+// console.log(concat);
 
-console.log(...concat, "seven");
+// console.log(...concat, "seven");
+
+// const obj1 = { x: 1, y: 2 };
+// const obj2 = { z: 3 };
+
+// const obj3 = { ...obj1, ...obj2 };
+// console.log(obj3);
+
+// let person = {
+//   name: "Thomas",
+//   age: 25,
+//   gender: "male",
+// };
+
+// const clone = { ...person, work: "Developer" };
+// console.log(clone);
+
+//problems
+
+let arr = [1, 2, 3];
+let arr2 = [4, 5];
+
+const user = {
+  name: "jen",
+  age: 22,
+};
+
+const arr3 = [...arr, ...arr2];
+console.log(arr3);
+
+const clone = { ...user };
+console.log(clone);
