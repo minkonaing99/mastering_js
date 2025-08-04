e9f612b5d9a17979245108b6a0d0bf7052dc87c9
Min Ko Naing <mkn.minko.naing99@gmail.com>
2025-08-04 18:25:56 +0700

    new

--- simple_js/spread_operator.js
+++ simple_js/spread_operator.js
@@ -1,11 +1,24 @@
 // Spread Operator
 // Function, Array, Object
 
-function giveMe4(a, b, c, d) {
-  console.log("a", a);
-  console.log("b", b);
-  console.log("c", c);
-  console.log("d", d);
-}
+// function giveMe4(a, b, c, d) {
+//   console.log("a", a);
+//   console.log("b", b);
+//   console.log("c", c);
+//   console.log("d", d);
+// }
 
-giveMe4(1, 2, 3, 4);
+// giveMe4(1, 2, 3, 4);
+
+// const colors = ["red", "green", "blue", "teal"];
+
+// giveMe4(...colors);
+// console.log(...colors);
+
+const strNums = ["one", "two", "three"];
+const moreStrNums = ["four", "five", "six"];
+
+const concat = [...strNums, ...moreStrNums];
+console.log(concat);
+
+console.log(...concat, "seven");
